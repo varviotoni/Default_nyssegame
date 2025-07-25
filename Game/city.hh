78572bf88ec4a8e9d0e3527fea4237f4f2f0c14e
hermes <tonivarvio@duck.com>
2025-07-25 14:46:55 +0300

    Added function declarations and implementations for city

--- Game/city.hh
+++ Game/city.hh
@@ -1,10 +1,25 @@
 #ifndef CITY_HH
 #define CITY_HH
+#include "interfaces/icity.hh"
 
 class city : public Interface::ICity
 {
 public:
     city();
+
+    // ICity interface
+public:
+    void setBackground(QImage &basicbackground, QImage &bigbackground) override;
+    void setClock(QTime clock) override;
+    void addStop(std::shared_ptr<Interface::IStop> stop) override;
+    void startGame() override;
+    void addActor(std::shared_ptr<Interface::IActor> newactor) override;
+    void removeActor(std::shared_ptr<Interface::IActor> actor) override;
+    void actorRemoved(std::shared_ptr<Interface::IActor> actor) override;
+    bool findActor(std::shared_ptr<Interface::IActor> actor) const override;
+    void actorMoved(std::shared_ptr<Interface::IActor> actor) override;
+    std::vector<std::shared_ptr<Interface::IActor> > getNearbyActors(Interface::Location loc) const override;
+    bool isGameOver() const override;
 };
 
 #endif // CITY_HH
