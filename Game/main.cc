c976ac6dbaa462a5fdfbfd06d1696e60cdf87dcd
hermes <tonivarvio@duck.com>
2025-07-30 17:46:42 +0300

    Added initialization of city, mainwindow and logic

--- Game/main.cc
+++ Game/main.cc
@@ -1,11 +1,23 @@
 #include <QApplication>
 #include "graphics/simplemainwindow.hh"
+#include "city.hh"
+#include "creategame.hh"
+#include "core/logic.hh"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(offlinedata);
+    auto icity=Interface::createGame();
+
+    std::shared_ptr<StudentSide::City> city =
+        std::dynamic_pointer_cast<StudentSide::City>(icity);
+
     CourseSide::SimpleMainWindow mainwindow;
+    auto logic = new CourseSide::Logic(&mainwindow);
+    logic->takeCity(icity);
+    logic->fileConfig();
+    logic->setTime(10,0);
     mainwindow.show();
 
     return a.exec();
